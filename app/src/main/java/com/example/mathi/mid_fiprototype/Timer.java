d6708c40b921a177efd7e557bd9124ce9c0a9386
Mathias Sebastian Johansen <msjo17@student.aau.dk>
2018-04-16 14:22:14 +0200

    Timer activity. Still WIP

---
+++ app/src/main/java/com/example/mathi/mid_fiprototype/Timer.java
@@ -0,0 +1,13 @@
+package com.example.mathi.mid_fiprototype;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Timer extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_timer);
+    }
+}
