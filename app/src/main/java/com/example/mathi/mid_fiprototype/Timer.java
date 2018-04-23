1e0e0df28fd0f4febe28bd321d96f313482c73a4
Mathias Sebastian Johansen <msjo17@student.aau.dk>
2018-04-23 10:48:16 +0200

    Changed Timer.java (Implemented threading. Crashes on t.start(); Brian, would you take a look at it?)

--- app/src/main/java/com/example/mathi/mid_fiprototype/Timer.java
+++ app/src/main/java/com/example/mathi/mid_fiprototype/Timer.java
@@ -1,5 +1,7 @@
 package com.example.mathi.mid_fiprototype;
 
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.CompoundButton;
@@ -25,69 +27,82 @@ public class Timer extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_timer);
         init();
-        timerIsRunning = true;
-        toggle = findViewById(R.id.toggle);
     }
     @Override
     protected void onStart(){
         super.onStart();
-        timerIsRunning = true;
+        final Handler handler = new Handler(){
+            @Override
+            public void handleMessage(Message msg){
+                timer.setText(Float.toString(timerTime));
+            }
+        };
+        Runnable r = new Runnable() {
+            @Override
+            public void run() {
+                switch(timerID){
+                    case 0:
+                        while(timerTime <= recievedTime1) {
+                            float timer1 = java.lang.System.currentTimeMillis();
+                            while(elapsedTime < 0.25){
+                                float timer2 = java.lang.System.currentTimeMillis();
+                                elapsedTime = timer1 - timer2;
+                                if(elapsedTime == 0.25){
+                                    timerTime += 0.25;
+                                }
+                                if(elapsedTime > 0.25){
+                                    elapsedTime = 0;
+                                }
+                            }
+                            if (timerTime == recievedTime1) {
+                                timerTime = 0;
+                                timerID = 1;
+                                handler.sendEmptyMessage(0);
+                                break;
+                            }
+                        }
+                        break;
+                    case 1:
+                        while(timerTime <= recievedTime2){
+                            float timer1 = java.lang.System.currentTimeMillis();
+                            while(elapsedTime < 0.25){
+                                float timer2 = java.lang.System.currentTimeMillis();
+                                elapsedTime = timer1 - timer2;
+                                if(elapsedTime == 0.25){
+                                    timerTime += 0.25;
+                                }
+                                if(elapsedTime > 0.25){
+                                    elapsedTime = 0;
+                                }
+                            }
+                            if(timerTime == recievedTime2){
+                                timerTime = 0;
+                                timerID = 0;
+                                handler.sendEmptyMessage(0);
+                                break;
+                            }
+                        }
+                        break;
+                }
+            }
+        };
+        final Thread t = new Thread(r);
         toggle.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 if (isChecked) {
-                    timerIsRunning = true;
+                    t.start();
                 } else {
-                    timerIsRunning = false;
+                    t.stop();
                 }
             }
         });
-        if(timerIsRunning){
-            runTime();
-        }
+        t.start();
     }
     public void init(){
         timer = findViewById(R.id.timer);
+        toggle = findViewById(R.id.toggle);
         recievedTime1 = MainActivity.getNumPickValue1();
         recievedTime2 = MainActivity.getNumPickValue2();
     }
-    public void runTime() {
-        switch(timerID){
-            case 0:
-                while(timerTime <= recievedTime1) {
-                    calcTime();
-                    if (timerTime == recievedTime1) {
-                        timerTime = 0;
-                        timerID = 1;
-                        break;
-                    }
-                }
-                break;
-            case 1:
-                while(timerTime <= recievedTime2){
-                    calcTime();
-                    if(timerTime == recievedTime2){
-                        timerTime = 0;
-                        timerID = 0;
-                        break;
-                    }
-                }
-                break;
-        }
-    }
-    public void calcTime(){
-        float timer1 = java.lang.System.currentTimeMillis();
-        while(elapsedTime < 0.25){
-            test = Float.toString(timerTime);
-            timer.setText(test);
-            float timer2 = java.lang.System.currentTimeMillis();
-            elapsedTime = timer1 - timer2;
-            if(elapsedTime == 0.25){
-                timerTime += 0.25;
-            }
-            if(elapsedTime > 0.25){
-                elapsedTime = 0;
-            }
-        }
-        timer.setText(Float.toString(timerTime));
-    }
+
 }
